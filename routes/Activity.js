95d9cb91466dfd6ef59ed16354b21ef407b7561e
RayenCW <mohamedrayen.dhraief@esprit.tn>
2024-04-04 12:59:21 +0100

    fatma changes

--- routes/Activity.js
+++ routes/Activity.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { getActivities ,deleteActivity ,updateActivity} from "../controllers/Activity.js";
 import { verifyToken } from "../middleware/auth.js";
-
+import { getActivities ,deleteActivity ,updateActivity,incrementParticipants, addActivityFeedback} from "../controllers/Activity.js";
 const router = express.Router();
 
 /* READ */
 router.get("/get", verifyToken, getActivities);
 router.delete('/remove/:id', deleteActivity);
 router.put('/update/:id', updateActivity);
+router.put('/updatePnb/:id',verifyToken, incrementParticipants);
+router.post('/addActivityFeedback/:id', verifyToken , addActivityFeedback);
 // router.get("/:userId/posts", verifyToken, getUserPosts);
 
 // /* UPDATE */
